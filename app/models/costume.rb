df8808e871e92e9018b3dc3d6fd50b0dc99d44a5
flatlandnick <nicholas.lunn@flatironschool.com>
2021-07-14 12:34:16 -0500

    commit tree 00dc89c9e92352cccfb87be137117d0918a59206

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,16 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+    
+    # attr_accessor :name, :image_url, :size, :price
+    # @@all = []
+    # def initialize
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+
+end
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,43 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+
+# naming conventions
+
+# what are we trying to do? 
+# create a table called costumes
+
+class CreateCostumes < ActiveRecord::Migration[5.2]
+    # def up
+    #     # method to be run when rake db:migrate is executed
+    # end
+
+    # def down
+    #     # method to be run when rake db:rollback is executed
+    # end
+
+    def change
+        # combines up and down
+        create_table :costumes do |t|
+            t.string :name
+            t.string :size
+            t.string :image_url
+            t.float :price 
+
+            t.timestamps
+        end
+    end
+
+end
+
+
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
